93b8dd3a136c718b0ef25b0e91c73dd474fbe9ee
Daniyal Tayyab <daniyaltayyab91@gmail.com>
2022-05-11 20:45:20 +0500

    responsive

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import tick from "../../assets/tick.png";
 import "./footer.css";
 import hang from "../../assets/hang.png";
+import twitter from "../../assets/twitter.png";
+import telegram from "../../assets/telegram.png";
+import reddit from "../../assets/reddit.png";
+import instagram from "../../assets/instagram.png";
+import github from "../../assets/github.png";
+import discord from "../../assets/discord.png";
+import medium from "../../assets/medium.png";
+import right from "../../assets/right.png";
 
 const Footer = () => {
   return (
@@ -79,26 +87,44 @@ const Footer = () => {
           <div className="footer-col">
             <div className="social-links">
               <a href="#">
-                <img src={tick} alt="tick" className="social-img" />
+                <img src={twitter} alt="tick" className="social-img" />
+                {/* <i class="fa-brands fa-twitter"></i> */}
               </a>
               <a href="#">
-                <img src={tick} alt="tick" className="social-img" />
+                <img src={telegram} alt="tick" className="social-img" />
               </a>
               <a href="#">
-                <img src={tick} alt="tick" className="social-img" />
+                <img src={reddit} alt="tick" className="social-img" />
               </a>
               <a href="#">
-                <img src={tick} alt="tick" className="social-img" />
+                <img src={instagram} alt="tick" className="social-img" />
               </a>
               <a href="#">
-                <img src={tick} alt="tick" className="social-img" />
+                <img src={github} alt="tick" className="social-img" />
               </a>
               <a href="#">
-                <img src={tick} alt="tick" className="social-img" />
+                <img src={discord} alt="tick" className="social-img" />
               </a>
               <a href="#">
-                <img src={tick} alt="tick" className="social-img" />
+                <img src={medium} alt="tick" className="social-img" />
               </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-row-lower">
+        <div className="lower-row-left">left</div>
+        <div className="lower-rwo-right">
+          <div className="lower-right-grid">
+            <div className="lower-right-logo-box">
+              <img className="footer-logo footer-blnc" src={hang} alt="logo" />
+              <div className="footer-blnc">$6.637</div>
+            </div>
+            <div>
+              <button className="footer-btn">
+                Buy CAKE{" "}
+                <img src={right} alt="right-arrow" className="right-arrow" />
+              </button>
             </div>
           </div>
         </div>
